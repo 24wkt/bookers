0d1ab3eccb04fbaa9358ad6a0a16bff3bb5f59ea
24wkt <05wkt38xy.waku@gmail.com>
2021-03-08 06:17:56 +0000

    ２回目のコミット

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,25 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.all
   end
 
   def show
   end
 
   def new
+    @books = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path(books.id)
+  end
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
