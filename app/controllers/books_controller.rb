96ecd1c8238895ca9905c6bb20a7fe65b6182c09
24wkt <05wkt38xy.waku@gmail.com>
2021-03-09 01:39:45 +0000

    3回目のコミット

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,36 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.all
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
-    @books = Book.new
+    @book = Book.new
   end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path(books.id)
+    redirect_to books_path
   end
+
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    blog.destroy
+    redirect_to book_path
   end
 
   private
